bd3156447edbe7592ac48e44a82fed9f7fd885b7
TheOndrej <38298439+TheOndrej@users.noreply.github.com>
2018-04-12 21:09:38 +0200

    Add files via upload

--- src/Main/me/Ondrej/AntiMove/Main.java
+++ src/Main/me/Ondrej/AntiMove/Main.java
@@ -1,6 +1,8 @@
 package me.Ondrej.AntiMove;
 
         import me.Ondrej.AntiMove.Listeners.Join;
+        import me.Ondrej.AntiMove.commands.Freeze;
+        import me.Ondrej.AntiMove.commands.Unfreeze;
         import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -12,6 +14,7 @@ public class Main extends JavaPlugin{
         getLogger().info("Plugin is enable");
         pl = this;
         onListeners();
+        onCommands();
     }
 
     @Override
@@ -21,5 +24,10 @@ public class Main extends JavaPlugin{
 
     private void onListeners(){
         getServer().getPluginManager().registerEvents(new Join(), this);
+    }
+
+    private void onCommands(){
+        getCommand("freeze").setExecutor(new Freeze());
+        getCommand("unfreeze").setExecutor(new Unfreeze());
     }
 }
